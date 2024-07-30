0cc316da683f4a6360fefc2b25c38940d605fe66
matthijs <72670748+Matthijs-Gorter@users.noreply.github.com>
2024-07-30 02:58:45 +0200

    made the DQL agent better
    Added target network
    Added a way to save and load a NN using pickle

--- agent.py
+++ agent.py
@@ -6,23 +6,52 @@ from game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
 from helper import plot
 import time
+import pickle
+import os
 
 
-MAX_MEMORY = 100_000
+MAX_MEMORY = 1_000_000
 BATCH_SIZE = 1000
 LR = 0.001
 
 class Agent:
-
     def __init__(self):
+        
         self.n_games = 0
-        self.epsilon = 0 # randomness
-        self.gamma = 0.9 # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.epsilon = 0  # randomness
+        self.gamma = 0.9  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
+        self.epsilon_decay = 0.995
+        self.min_epsilon = 0.01
+
         self.model = Linear_QNet(11, 256, 3)
-        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+        self.target_model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, self.target_model, lr=LR, gamma=self.gamma)
+        
+        self.update_target_network()
+        self.load_model_and_memory()
+
+def save_model_and_memory(self):
+    self.model.save()
+    with open('./model/memory.pkl', 'wb') as f:
+        pickle.dump(self.memory, f)
+    with open('./model/n_games.pkl', 'wb') as f:
+        pickle.dump(self.n_games, f)
+
+def load_model_and_memory(self):
+    if os.path.exists('./model/model.pth'):
+        self.model.load('./model/model.pth')
+    if os.path.exists('./model/memory.pkl'):
+        with open('./model/memory.pkl', 'rb') as f:
+            self.memory = pickle.load(f)
+    if os.path.exists('./model/n_games.pkl'):
+        with open('./model/n_games.pkl', 'rb') as f:
+            self.n_games = pickle.load(f)
 
 
+    def update_target_network(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+        
     def get_state(self, game):
         head = game.snake[0]
         point_l = Point(head.x - 20, head.y)
@@ -85,12 +114,11 @@ class Agent:
 
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
-
+        
     def get_action(self, state):
-        # random moves: tradeoff exploration / exploitation
-        self.epsilon = 80 - self.n_games
-        final_move = [0,0,0]
-        if random.randint(0, 200) < self.epsilon:
+        self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
+        final_move = [0, 0, 0]
+        if random.random() < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
         else:
@@ -98,9 +126,7 @@ class Agent:
             prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
-
         return final_move
-
 
 def train():
     start_time = time.time()
@@ -110,30 +136,19 @@ def train():
     record = 0
     agent = Agent()
     game = SnakeGameAI()
-    visualise = False;
+    visualise = False
     last_10_scores = deque(maxlen=10)
 
     while True:
-        # get old state
         state_old = agent.get_state(game)
-
-        # get move
         final_move = agent.get_action(state_old)
-
-        # perform move and get new state
         reward, done, score = game.play_step(final_move, visualise)
         state_new = agent.get_state(game)
 
-        # train short memory
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
-
-        # remember
         agent.remember(state_old, final_move, reward, state_new, done)
 
-
-
         if done:
-            # train long memory, plot result
             game.reset()
             agent.n_games += 1
             agent.train_long_memory()
@@ -144,27 +159,24 @@ def train():
 
             plot_scores.append(score)
             last_10_scores.append(score)
-            
-            # Calculate the average of the last 10 scores
             if len(last_10_scores) == 10:
                 mean_last_10_scores = sum(last_10_scores) / 10
             else:
                 mean_last_10_scores = sum(last_10_scores) / len(last_10_scores)
-            
             plot_mean_scores.append(mean_last_10_scores)
+
+            if agent.n_games % 10 == 0:
+                agent.update_target_network()
+                agent.save_model_and_memory()
+
             
-            end_time = time.time()
-
-            print('Game:', agent.n_games, '\tScore:', score, '\tRecord:', record, "\tElapsed time (s):", end_time - start_time)
-
-            if agent.n_games == 200:
                 end_time = time.time()
-                elapsed_time = end_time - start_time
-                print('Game:', agent.n_games, '\tScore:', score, '\tRecord:', record, "\tElapsed time (s):", elapsed_time)
+                print('Game:', agent.n_games, '\tScore:', score, '\tRecord:', record, "\tElapsed time (s):", end_time - start_time)
+
+
+            if agent.n_games >= 200:
                 plot(plot_scores, plot_mean_scores)
                 visualise = True
-
-
 
 
 if __name__ == '__main__':
